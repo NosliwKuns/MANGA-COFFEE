67c91775059903948bfa34837b7c9b42e61fcefa
sky <ant_664@hotmail.com>
2022-07-07 01:11:45 -0500

    entorn variables config

--- Servers/src/index.js
+++ Servers/src/index.js
@@ -9,8 +9,8 @@ const app_js_1 = __importDefault(require("./app.js"));
 // read localhost from variables and ports
 
 const host = process.env.HOST || '0.0.0.0';
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-app_js_1.default.listen(PORT, host, () => {
-    console.log('app listening on port ' + PORT);
+app_js_1.default.listen(port, host, () => {
+    console.log('app listening on port ' + port);
 });
