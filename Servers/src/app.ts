96580860bb17924a4b80985f141f997dbc05ecb7
sky <ant_664@hotmail.com>
2022-07-07 13:57:29 -0500

    typescript conf cors and heroku deployment

--- Servers/src/app.ts
+++ Servers/src/app.ts
@@ -2,12 +2,33 @@ import express from'express';
 import routes from './routes/index';
 import './db.js';
 
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
 
 const server = express();
 
 server.use(express.json());
 
 server.use('/api', routes);
+
+
+//-------------------cors config--------------------//
+
+server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+server.use(bodyParser.json({ limit: '50mb' }));
+server.use(cookieParser());
+server.use(morgan('dev'));
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+//
 
 
 server.use((err:any, _req:any, res:any, _next:any) => {
--- Servers/src/index.ts
+++ Servers/src/index.ts
@@ -1,6 +1,17 @@
 import server from './app.js';
 
-const PORT = process.env.PORT || 3000
-server.listen(PORT, ()=> {
-    console.log('app listening on port ' + PORT)
-})
+// const PORT = process.env.PORT || 3000
+// server.listen(PORT, ()=> {
+//     console.log('app listening on port ' + PORT)
+// })
+
+var express = require('express');
+var app     = express();
+const cors = require('cors');
+server.use(cors());
+
+app.set('port', (process.env.PORT || 5000));
+
+server.listen(app.get('port'), () => {
+    console.log('app listening on port ');
+});
