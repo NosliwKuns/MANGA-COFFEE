cc2883aa2f7fb723b1e1de8fc72683312ffc7015
sky <ant_664@hotmail.com>
2022-07-07 00:06:50 -0500

    deployment backend config

--- Servers/src/index.js
+++ Servers/src/index.js
@@ -2,9 +2,15 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_js_1 = __importDefault(require("./app.js"));
+
+// read localhost from variables and ports
+
+const host = process.env.HOST || '0.0.0.0';
 const PORT = process.env.PORT || 3000;
-app_js_1.default.listen(PORT, () => {
+
+app_js_1.default.listen(PORT, host, () => {
     console.log('app listening on port ' + PORT);
 });
