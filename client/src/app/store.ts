2ce1b24188ed16688accf72609b86ac386d46eb1
Your Namesergio-jc <sergiojara0609@gmail.com>
2022-07-07 01:36:48 -0500

    feat(Cards components & AppThunk)

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -11,9 +11,7 @@ const store = configureStore({
 })
 
 export default store
-  // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
 export type AppThunk<ReturnType = void > = ThunkAction < 
 ReturnType,
