ddb3fb7e353a1fdb3aad70b1d8a3bd5b98429896
EmaPaul <enmanuelpaulcarrillo.rakata900@gmail.com>
2022-07-08 21:04:53 -0500

    filterGenre

--- Servers/src/routes/index.ts
+++ Servers/src/routes/index.ts
@@ -4,6 +4,7 @@ import PostManga from './Mangas/PostManga/index';
 import GetFindAll from './Mangas/GetFindAll/index';
 import GetById from './Mangas/GetById/index';
 import DeleteById from './Mangas/DeleteById/index';
+import FilterByGenre from './Mangas/FilterByGenre/index';
 import PostUserCreated from './user/PostUserCreated/index';
 import PostUserinit from './user/PostUserinit/index';
 import GetByIdUser from './user/GetByIdUser/index'
@@ -16,6 +17,7 @@ router.use('/manga', GetByName);
 router.use('/manga', GetById);
 router.use('/manga', PostManga);
 router.use('/manga', DeleteById);
+router.use('/manga',FilterByGenre)
 
 router.use('/user', PostUserCreated);
 router.use('/user', PostUserinit);
