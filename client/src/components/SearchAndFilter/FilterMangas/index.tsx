db1e9b20b6d8997187fae34a2551e951e9a4c6e7
sky <ant_664@hotmail.com>
2022-07-11 08:45:49 -0500

    fix(client/mangaSilce): fix initial state and routes

--- client/src/components/SearchAndFilter/FilterMangas/index.tsx
+++ client/src/components/SearchAndFilter/FilterMangas/index.tsx
@@ -1,32 +1,20 @@
 import { useId } from "react";
-import { useState } from 'react';
-import { fetchMangaByGenres } from "../../../features/manga/mangaSlice";
+import { useEffect } from 'react';
+import { fetchMangaByGenres, fetchGetGenres } from "../../../features/manga/mangaSlice";
 import { useAppDispatch , useAppSelector } from "../../../app/hooks";
 
 const FilterMangas = () => {
 
   const id = useId();
   const dispatch = useAppDispatch();
-  const { mangas } = useAppSelector(state => state.mangas);
-  const data : Array<string> = mangas.flatMap(e => e.genres); 
-  const dataArr = new Set(data);
-  const genres : Array<string> = [...dataArr];
+  const { genres } = useAppSelector(state => state.mangas);
 
-  let b : Array<string> = ['action', 'adventure', 'fantasy', 'comedy', 'drama', 'mystery', 'romance', 'Drama', 'Action', 'horror', 'sciFi', 'school life']
-  console.log(genres);
+  useEffect(() => {
+    dispatch(fetchGetGenres())
+  },[])
 
-  /* const [change, setChange] = useState<any>([]) */
 
   const handleClick = (event: any) => {
-    console.log(event.target.innerText);
-
-    /* const newArr = !change.includes(event.target.innerText)
-      ? [...change, event.target.innerText]
-      : [...change.filter((genre : string) => genre !== event.target.innerText)]
-  
-    setChange(newArr) */
-    console.log(event.target.innerText)
-  
     dispatch(fetchMangaByGenres(event.target.innerText))
   }
 
