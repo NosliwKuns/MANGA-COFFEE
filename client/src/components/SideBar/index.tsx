67c52ab44ef081af549ff2e68dba704127ad4feb
Rochi Albani <rocio.albani@hotmail.com>
2022-07-11 03:37:31 -0300

    fix(components/SideBar) completed

--- client/src/components/SideBar/index.tsx
+++ client/src/components/SideBar/index.tsx
@@ -1,13 +1,21 @@
 import { Link } from "react-router-dom";
-import { IoHomeOutline } from 'react-icons/io5'
-import { MdOutlineStorefront } from 'react-icons/md'
+import { BsFillHouseDoorFill } from 'react-icons/bs'
+import { IoStorefront } from 'react-icons/io5'
+import { AiTwotoneAppstore } from "react-icons/ai";
+import { MdLibraryBooks } from "react-icons/md";
+import { GoFlame } from "react-icons/go";
+import { RiHistoryLine } from "react-icons/ri";
 
 
 const SideBar = () =>{
     return(
         <div>
-            <Link to='/'><p> <IoHomeOutline /> Home</p></Link>
-            <Link to='/store'><p> <MdOutlineStorefront /> Store</p></Link>
+            <Link to='/'><p><BsFillHouseDoorFill /> Home</p></Link>
+            <Link to='/categories'><p><AiTwotoneAppstore /> Categories</p></Link>
+            <Link to='/newreleases'><p><MdLibraryBooks/> New Releases</p></Link>
+            <Link to='/popular'><p><GoFlame/> Popular</p></Link>
+            <Link to='/store'><p><IoStorefront/> Store</p></Link>
+            <Link to='/history'><p><RiHistoryLine/> History</p></Link>
         </div>
     )
 }
