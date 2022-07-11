818d2c2aecb7ef9149ee8fea796bec24a152165c
andreacco <andreacc9199@gmail.com>
2022-07-11 10:44:37 -0400

    fix(component/sideBar dispatch action fetch all mangas when clicking home button on the sideBar) completed)

--- client/src/components/SideBar/index.tsx
+++ client/src/components/SideBar/index.tsx
@@ -5,12 +5,19 @@ import { AiTwotoneAppstore } from "react-icons/ai";
 import { MdLibraryBooks } from "react-icons/md";
 import { GoFlame } from "react-icons/go";
 import { RiHistoryLine } from "react-icons/ri";
+import { useAppDispatch } from "../../app/hooks";
+import {fetchAllManga} from '../../features/manga/mangaSlice'
+
 
 
 const SideBar = () =>{
+    const dispatch = useAppDispatch()
+    const handleClick = () => {
+        dispatch(fetchAllManga())
+    }
     return(
         <div>
-            <Link to='/'><p><BsFillHouseDoorFill /> Home</p></Link>
+            <Link to='/'><p onClick={() => handleClick()}><BsFillHouseDoorFill /> Home</p></Link>
             <Link to='/categories'><p><AiTwotoneAppstore /> Categories</p></Link>
             <Link to='/newreleases'><p><MdLibraryBooks/> New Releases</p></Link>
             <Link to='/popular'><p><GoFlame/> Popular</p></Link>
