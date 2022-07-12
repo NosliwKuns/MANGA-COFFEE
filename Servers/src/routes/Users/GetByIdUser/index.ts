cde60e7d4f62c2bd43c09139dab7ab5d2eb88bcf
Gabriel-2310 <luisgamorales1@gmail.com>
2022-07-11 19:01:48 -0500

    rafactor(server): modificacion get detail

--- Servers/src/routes/Users/GetByIdUser/index.ts
+++ Servers/src/routes/Users/GetByIdUser/index.ts
@@ -4,12 +4,12 @@ import User from '../../../models/Users/User.js';
 import ReadTokenData from '../../../controles/ReadTokenData/index.js';
 const router = Router();
 
-router.get('/', passport.authenticate("jwt", { session: false }), async(req, res, next) => {
+router.get('/detail', passport.authenticate("jwt", { session: false }), async(req, res, next) => {
     console.log('GetByIdUser')
     const {authorization} = req.headers;    
     try {        
-        const data= ReadTokenData(authorization)
-        const user = await User.findById(data.id)
+        const data= ReadTokenData(authorization);
+        const user = await User.findById(data.id);
         res.status(200).json(user)
     } catch (error) {
         next(error)
