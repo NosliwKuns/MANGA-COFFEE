e7c7a5759092ba258649976f3ac1d49c9a9c1454
Eduardo <70926975+librotero@users.noreply.github.com>
2022-07-12 15:59:14 -0300

    putManga

--- Servers/src/routes/index.ts
+++ Servers/src/routes/index.ts
@@ -5,6 +5,8 @@ import GetFindAll from './Mangas/GetFindAll/index';
 import GetById from './Mangas/GetById/index';
 import DeleteById from './Mangas/DeleteById/index';
 import FilterByGenre from './Mangas/FilterByGenre/index';
+import PutManga from './Mangas/PutManga/index';
+
 import PostUserCreated from './Users/PostUserCreated/index';
 import PostUserinit from './Users/PostUserinit/index';
 import GetByIdUser from './Users/GetByIdUser/index';
@@ -30,6 +32,7 @@ router.use('/manga', GetByName);
 router.use('/manga', GetById);
 router.use('/manga', PostManga);
 router.use('/manga', DeleteById);
+router.use('/manga', PutManga);
 
 router.use('/user', DeleteUsers);
 router.use('/user', PostUserCreated);
