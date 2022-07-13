b576e322ce5cecc62e8106badf3a3a408486677d
Andreeacco <andreacc9199@gmail.com>
2022-07-12 20:49:58 -0400

    fix(components/detail button post favorites)

--- client/src/features/user/userSlice.ts
+++ client/src/features/user/userSlice.ts
@@ -124,8 +124,12 @@ export const singUpUser = (user: InitialState): AppThunk => {
 
 export const setDetailUser = (id:string , headers: object):AppThunk => {
   return async () => {
-    const {data} = await axios.get(`http://localhost:5000/api/user/${id}`, headers )
-    // console.log(data)
+    try{
+      const {data} = await axios.get(`http://localhost:5000/api/user/detail`, headers )
+      console.log(data)
+    } catch  (e) {
+      console.log('hola')
+    }
   }
 }
 
