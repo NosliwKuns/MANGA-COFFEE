249e442f78cb6d451f40c6166bf0695f8bf4cdd3
Gabriel-2310 <luisgamorales1@gmail.com>
2022-07-16 17:21:32 -0500

    refactor(server): refactor PostBuyStripe

--- Servers/src/routes/Products/PostBuyStripe/index.ts
+++ Servers/src/routes/Products/PostBuyStripe/index.ts
@@ -4,6 +4,8 @@ import passport from "passport";
 import ReadTokenData from '../../../controles/Token/ReadTokenData';
 import Product from '../../../models/Products/index';
 import User from '../../../models/Users/User';
+//import sendEmail from '../../../controles/Email/SendEmail';
+//import NotificationBuy from '../../../controles/Email/Template/NotificacionCompra';
 const router = Router();
 
 const stripe = new Stripe(
@@ -13,55 +15,53 @@ const stripe = new Stripe(
 
 router.post("/checkout/:idCompra", passport.authenticate("jwt", { session: false }), async (req, res) => {
   try {
-    const { quantity, idProduct } = req.body;
+    const { adrress, product, InfoComprador } = req.body;
     const {idCompra} = req.params;
     const {authorization} = req.headers; 
 
-    const data= ReadTokenData(authorization);
-
-    const product = await Product.findById(idProduct);        
-    if (product){
-    let stock = product.stock - quantity;
-    await Product.findByIdAndUpdate((idProduct), {stock: stock})
-
-    let amount: number = product.price * quantity
-
+    let amount: number = 0;
+    let ArrrayProducts: [Object] = [{}];
+    product.forEach( async (element: any) =>{
+      const producto = await Product.findById(element.idProduct);        
+      if (producto){
+        let stock = producto.stock - element.quantity;
+        await Product.findByIdAndUpdate((element.idProduct), {stock: stock});
+        let totProduct = producto.price * element.quantity * 100;
+        amount += totProduct;
+        let DetailProduct = {
+          idProduct: element.idProduct,
+          name: producto.name,
+          price: producto.price,
+          quantity: element.quantity,
+          totProduct: totProduct, 
+        }
+        Object.keys(ArrrayProducts[0]).length?ArrrayProducts.push(DetailProduct): ArrrayProducts = [DetailProduct]   
+      }
+    });
     const payment = await stripe.paymentIntents.create({
       amount: amount,
       currency: "USD",
-      description: product.description,
       payment_method: idCompra,
       confirm: true,
     });  
-    console.log(payment);
     const compra = {
       idCompra: idCompra,
-      produtcs: [{
-        idProduct: idProduct,
-        name: product.name,
-        price: product.price,
-        quantity: quantity 
-      }],
+      produtcs: ArrrayProducts,
       total : amount,
-      // adrress:{
-      //   postalCode : ,
-      //   country : String,
-      //   direction : String,
-      //   reference : String
-      // },
-      // name: String,
-      // lastName: ,
-      // telephone: ,
-      // method: ,
-      // email: 
+      adrress: adrress, 
+      InfoComprador: InfoComprador,
     }
-    await User.findByIdAndUpdate((data.id), {historyBuy: [compra]})
+    console.log(payment)
+    const data= ReadTokenData(authorization);
+    await User.findByIdAndUpdate((data.id), {$push:{historyBuy: [compra]}})
+    //let template = NotificationBuy()
+    //sendEmail(data.email, 'Notificacion de compra', template)
     res.send({ message: "Successull payment" });
-  }
+  
   } catch (error) {
     console.log(error);
-    res.json({ mirar:'este es el lio', message: error.raw.message });
+    res.json({leer: 'error de stripe', message: error.raw.message });
   }
 });
 
-export default router;
+export default router; 
