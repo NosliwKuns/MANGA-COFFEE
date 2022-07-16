eb1b8364b42d831185d1ae558c92a8c584bab4e8
Gabriel-2310 <luisgamorales1@gmail.com>
2022-07-16 09:25:11 -0500

    refactor(PostBuyStripe): modificar stock products, agregar hirtoryBuy a user

--- Servers/src/routes/Products/PostBuyStripe/index.js
+++ Servers/src/routes/Products/PostBuyStripe/index.js
@@ -17,31 +17,56 @@ const stripe_1 = __importDefault(require("stripe"));
 const passport_1 = __importDefault(require("passport"));
 const ReadTokenData_1 = __importDefault(require("../../../controles/Token/ReadTokenData"));
 const index_1 = __importDefault(require("../../../models/Products/index"));
+const User_1 = __importDefault(require("../../../models/Users/User"));
 const router = (0, express_1.Router)();
 const stripe = new stripe_1.default("sk_test_51LLrJiAaJyGKFRYYchn8r6wj05opINEVucofBXXorZQWhuq1zFJ1FW3Ys134xp4FuqnQynqh7CaQ6Rhks29Fck4t00fvKC5c6E", { apiVersion: "2020-08-27" });
 router.post("/checkout/:idCompra", passport_1.default.authenticate("jwt", { session: false }), (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        console.log('holaaaaa');
         const { quantity, idProduct } = req.body;
         const { idCompra } = req.params;
         const { authorization } = req.headers;
         const data = (0, ReadTokenData_1.default)(authorization);
         const product = yield index_1.default.findById(idProduct);
-        let amount = product.price * quantity * 100;
-        console.log(product.price);
-        const payment = yield stripe.paymentIntents.create({
-            amount: amount,
-            currency: "USD",
-            description: product === null || product === void 0 ? void 0 : product.description,
-            payment_method: idCompra,
-            confirm: true,
-        });
-        console.log(payment);
-        res.send({ message: "Successull payment" });
+        if (product) {
+            let stock = product.stock - quantity;
+            yield index_1.default.findByIdAndUpdate((idProduct), { stock: stock });
+            let amount = product.price * quantity;
+            const payment = yield stripe.paymentIntents.create({
+                amount: amount,
+                currency: "USD",
+                description: product.description,
+                payment_method: idCompra,
+                confirm: true,
+            });
+            console.log(payment);
+            const compra = {
+                idCompra: idCompra,
+                produtcs: [{
+                        idProduct: idProduct,
+                        name: product.name,
+                        price: product.price,
+                        quantity: quantity
+                    }],
+                total: amount,
+                // adrress:{
+                //   postalCode : ,
+                //   country : String,
+                //   direction : String,
+                //   reference : String
+                // },
+                // name: String,
+                // lastName: ,
+                // telephone: ,
+                // method: ,
+                // email: 
+            };
+            yield User_1.default.findByIdAndUpdate((data.id), { historyBuy: [compra] });
+            res.send({ message: "Successull payment" });
+        }
     }
     catch (error) {
         console.log(error);
-        res.json({ message: error.raw.message });
+        res.json({ mirar: 'este es el lio', message: error.raw.message });
     }
 }));
 exports.default = router;
--- Servers/src/routes/Products/PostBuyStripe/index.ts
+++ Servers/src/routes/Products/PostBuyStripe/index.ts
@@ -3,6 +3,7 @@ import Stripe from "stripe";
 import passport from "passport";
 import ReadTokenData from '../../../controles/Token/ReadTokenData';
 import Product from '../../../models/Products/index';
+import User from '../../../models/Users/User';
 const router = Router();
 
 const stripe = new Stripe(
@@ -12,27 +13,54 @@ const stripe = new Stripe(
 
 router.post("/checkout/:idCompra", passport.authenticate("jwt", { session: false }), async (req, res) => {
   try {
-    console.log('holaaaaa')
     const { quantity, idProduct } = req.body;
     const {idCompra} = req.params;
     const {authorization} = req.headers; 
 
     const data= ReadTokenData(authorization);
-    const product = await Product.findById(idProduct);
-    let amount = product.price * quantity * 100
-    console.log(product.price)
+
+    const product = await Product.findById(idProduct);        
+    if (product){
+    let stock = product.stock - quantity;
+    await Product.findByIdAndUpdate((idProduct), {stock: stock})
+
+    let amount: number = product.price * quantity
+
     const payment = await stripe.paymentIntents.create({
       amount: amount,
       currency: "USD",
-      description: product?.description,
+      description: product.description,
       payment_method: idCompra,
       confirm: true,
-    });
+    });  
     console.log(payment);
+    const compra = {
+      idCompra: idCompra,
+      produtcs: [{
+        idProduct: idProduct,
+        name: product.name,
+        price: product.price,
+        quantity: quantity 
+      }],
+      total : amount,
+      // adrress:{
+      //   postalCode : ,
+      //   country : String,
+      //   direction : String,
+      //   reference : String
+      // },
+      // name: String,
+      // lastName: ,
+      // telephone: ,
+      // method: ,
+      // email: 
+    }
+    await User.findByIdAndUpdate((data.id), {historyBuy: [compra]})
     res.send({ message: "Successull payment" });
+  }
   } catch (error) {
     console.log(error);
-    res.json({ message: error.raw.message });
+    res.json({ mirar:'este es el lio', message: error.raw.message });
   }
 });
 
