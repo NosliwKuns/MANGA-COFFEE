e7cc2dde47b7b0f0f110e37fae1eea5bf66f39a9
Rochi Albani <rocio.albani@hotmail.com>
2022-07-21 16:33:56 -0300

    fix(/component/chat) almost completed

--- client/src/components/Chat/Chat.tsx
+++ client/src/components/Chat/Chat.tsx
@@ -1,165 +1,89 @@
 import io from 'socket.io-client';
-import ChatConfig from './index'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import  ScrollToBottom from "react-scroll-to-bottom"
 const socket = io('http://localhost:5000');
 
 const Chat = () => {
+  const [currentMessage, setCurrentMessage] = useState("");
   const [username, setUsername] = useState("");
-  const [room, setRoom] = useState("");
-  const [showChat, setShowChat] = useState(false);
+  const [messageList, setMessageList] = useState<Array<any>>([]);
+  console.log('SOY MESSAGE LIIIIIST',);
+  
 
-  // const joinRoom = () => {
-  //   if (username !== "" && room !== "") {
-  //     socket.emit("join_room", room);
-  //     setShowChat(true);
-  //   }
-  // };
+  const sendMessage = () => {
+    if (currentMessage !== "") {
+        const messageData = {
+        author: username,
+        message: currentMessage,
+        time:
+            new Date(Date.now()).getHours() +
+            ":" +
+            new Date(Date.now()).getMinutes(),
+        };
 
+        socket.emit("send_message", messageData);
+        setMessageList( [...messageList, messageData]);
+        
+      setCurrentMessage("");
+    }
+  };
+
+  useEffect(() => {
+    socket.on("receive_message", (data : any) => {
+      setMessageList([...messageList, data]);
+    });
+  }, [socket]);
+  
   return (
-    <div className="App">
-      {!showChat ? (
-        <div className="joinChatContainer">
-          <h3>Join A Chat</h3>
-          <input
+    <div>
+      <div>
+        <p>Live Chat</p>
+      </div>
+        <ScrollToBottom>
+          {messageList.map((messageContent : any) => {
+            return (
+              <div
+                id={username === messageContent.author ? "you" : "other"}
+              >
+                <div>
+                  <div>
+                    <p>{messageContent.message}</p>
+                  </div>
+                  <div>
+                    <p id="time">{messageContent.time}</p>
+                    <p id="author">{messageContent.author}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </ScrollToBottom>
+      <input
             type="text"
             placeholder="Nickname..."
-            onChange={(event) => {
-              setUsername(event.target.value);
+            onChange={(e) => {
+              setUsername(e.target.value);
             }}
           />
-          <input
-            type="text"
-            placeholder="Room ID..."
-            onChange={(event) => {
-              setRoom(event.target.value);
-            }}
-          />
-          <button onClick={joinRoom}>Join A Room</button>
-        </div>
-      ) : (
-        <ChatConfig socket={socket} username={username} room={room} />
-      )}
+    <div>
+      <div>
+      <div>
+        <input
+          type="text"
+          value={currentMessage}
+          placeholder="Message..."
+          onChange={(e) => {
+            setCurrentMessage(e.target.value);
+          }}
+          onKeyPress={(event) => {
+            event.key === "Enter" && sendMessage();
+          }}
+        />
+        <button onClick={sendMessage}>Send</button>
+      </div>
+      </div>
+    </div>
     </div>
   );
 }
-  
   export default Chat;
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // import { useId, useState } from 'react';
-    // import { FaUserCircle } from 'react-icons/fa';
-    // import moment from 'moment';
-    // import { useAppDispatch, useAppSelector } from '../../app/hooks';
-    // import { fetchGlobalChat } from '../../features/ChatSlice/ChatSlice';
-    // import '../../../scss/Details/Comments.scss'
-
-
-
-  // const id = useId();
-  // const dispatch = useAppDispatch();
-  // const sms = useAppSelector(state => state.chat)
-  // const [input, setInput] = useState<any>({
-  //   name: 'Me',
-  //   id: id,
-  //   message: '',
-  //   time : ''
-  // });
- 
-  // let currentTime = moment().format();
-
-  // const timeAgo = (value : string) => {
-  //   return moment.utc(value).local().startOf('seconds').fromNow();
-  // };
-  
-  // const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
-  //   const { name, value } = e.currentTarget
-  //   setInput({
-  //     ...input,
-  //     [name] : value,
-  //     time: currentTime
-  //   })
-  // };
-
-  // const handleSubmit = (e: React.FormEvent) => {
-  //   e.preventDefault();
-  //   if(!input.message) {
-  //     return alert('Invalid action')
-  //   }
-  //   dispatch(fetchGlobalChat(input))
-  //   setInput({
-  //     name : 'Me',
-  //     id : id,
-  //     message : '',
-  //     time : ''
-  //   })
-  // };
-
-  // const arr = [...sms.chat]
-  // const myArray = arr.flatMap(e => e)
-  // console.log(myArray, 'flatmap');
-
-
-
-
-
-  // <div className="comments-container">
-  //   {
-  //     myArray.map((comment : any) => {
-  //       return (
-  //         <>
-  //           <div 
-  //             className="comment"
-  //             key={comment._id}
-  //           >
-  //             <FaUserCircle
-  //               size={44}
-  //             />
-  //             <section>
-  //               <h4>{comment.name}<span> {`${comment.time ? timeAgo(comment.time) : "2days ago"}`}</span></h4>
-  //               <p>{comment.message}</p>
-  //               <h5>REPLY</h5>
-  //             </section>
-  //           </div>
-  //           <span className="separator"></span>
-  //         </>
-  //       )
-  //     })
-  //   }
-  //   <section className="title-text">
-  //     <h2>Chat</h2>
-  //     <h2>{myArray.length}</h2>
-  //   </section>
-  //   <div className="comment">
-  //     <FaUserCircle
-  //       size={44}
-  //     />
-  //     <div className="input-text">
-  //       <form 
-  //         onSubmit={handleSubmit}
-  //       >
-  //         <input 
-  //           type="text" 
-  //           id="message" 
-  //           name="message" 
-  //           value={input.message}
-  //           onChange={handleChange}
-  //           placeholder="Chat with others..."/>
-  //         <section>
-  //           <button>Cancel</button>
-  //           <button type="submit">Comment</button>
-  //         </section>
-  //       </form>
-  //     </div>
-  //   </div>
-  // </div>
--- client/src/components/Chat/index.tsx
+++ client/src/components/Chat/index.tsx
@@ -1,77 +1,77 @@
-import  ScrollToBottom from "react-scroll-to-bottom";
-import { useState, useEffect } from 'react';
+// import  ScrollToBottom from "react-scroll-to-bottom";
+// import { useState, useEffect } from 'react';
 
-const ChatConfig = ({ socket, username, room } : any)=> {
-    const [currentMessage, setCurrentMessage] = useState("");
-    const [messageList, setMessageList] = useState<Array<any>>([]);
+// const ChatConfig = ({ socket, username, room } : any)=> {
+//     const [currentMessage, setCurrentMessage] = useState("");
+//     const [messageList, setMessageList] = useState<Array<any>>([]);
 
-    const sendMessage = async () => {
-    if (currentMessage !== "") {
-        const messageData = {
-        room: room,
-        author: username,
-        message: currentMessage,
-        time:
-            new Date(Date.now()).getHours() +
-            ":" +
-            new Date(Date.now()).getMinutes(),
-        };
+//     const sendMessage = async () => {
+//     if (currentMessage !== "") {
+//         const messageData = {
+//         room: room,
+//         author: username,
+//         message: currentMessage,
+//         time:
+//             new Date(Date.now()).getHours() +
+//             ":" +
+//             new Date(Date.now()).getMinutes(),
+//         };
 
-        await socket.emit("send_message", messageData);
-        setMessageList((list) => [...list, messageData]);
-      setCurrentMessage("");
-    }
-  };
+//         await socket.emit("send_message", messageData);
+//         setMessageList((list) => [...list, messageData]);
+//       setCurrentMessage("");
+//     }
+//   };
 
-  useEffect(() => {
-    socket.on("receive_message", (data : any) => {
-      setMessageList((list) => [...list, data]);
-    });
-  }, [socket]);
+//   useEffect(() => {
+//     socket.on("receive_message", (data : any) => {
+//       setMessageList((list) => [...list, data]);
+//     });
+//   }, [socket]);
 
-  return (
-    <div className="chat-window">
-      <div className="chat-header">
-        <p>Live Chat</p>
-      </div>
-      <div className="chat-body">
-        <ScrollToBottom className="message-container">
-          {messageList.map((messageContent : any) => {
-            return (
-              <div
-                className="message"
-                id={username === messageContent.author ? "you" : "other"}
-              >
-                <div>
-                  <div className="message-content">
-                    <p>{messageContent.message}</p>
-                  </div>
-                  <div className="message-meta">
-                    <p id="time">{messageContent.time}</p>
-                    <p id="author">{messageContent.author}</p>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
-        </ScrollToBottom>
-      </div>
-      <div className="chat-footer">
-        <input
-          type="text"
-          value={currentMessage}
-          placeholder="Hey..."
-          onChange={(event) => {
-            setCurrentMessage(event.target.value);
-          }}
-          onKeyPress={(event) => {
-            event.key === "Enter" && sendMessage();
-          }}
-        />
-        <button onClick={sendMessage}>&#9658;</button>
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div className="chat-window">
+//       <div className="chat-header">
+//         <p>Live Chat</p>
+//       </div>
+//       <div className="chat-body">
+//         <ScrollToBottom className="message-container">
+//           {messageList.map((messageContent : any) => {
+//             return (
+//               <div
+//                 className="message"
+//                 id={username === messageContent.author ? "you" : "other"}
+//               >
+//                 <div>
+//                   <div className="message-content">
+//                     <p>{messageContent.message}</p>
+//                   </div>
+//                   <div className="message-meta">
+//                     <p id="time">{messageContent.time}</p>
+//                     <p id="author">{messageContent.author}</p>
+//                   </div>
+//                 </div>
+//               </div>
+//             );
+//           })}
+//         </ScrollToBottom>
+//       </div>
+//       <div className="chat-footer">
+//         <input
+//           type="text"
+//           value={currentMessage}
+//           placeholder="Message..."
+//           onChange={(event) => {
+//             setCurrentMessage(event.target.value);
+//           }}
+//           onKeyPress={(event) => {
+//             event.key === "Enter" && sendMessage();
+//           }}
+//         />
+//         <button onClick={sendMessage}>&#9658;</button>
+//       </div>
+//     </div>
+//   );
+// }
 
-export default ChatConfig;
+// export default ChatConfig;
