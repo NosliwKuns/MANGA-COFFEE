974c01b3d8a8e623b92ab40464e510b6d5ed0b9f
Gabriel-2310 <luisgamorales1@gmail.com>
2022-07-21 19:27:22 -0500

    correcion regex send fin all admin

--- Servers/src/routes/Users/AdminPostSendMailFindAll/index.ts
+++ Servers/src/routes/Users/AdminPostSendMailFindAll/index.ts
@@ -14,8 +14,9 @@ router.post('/adminmails', passport.authenticate("jwt", { session: false }), asy
         const userAdmin = await User.findById(data.id);
         if (userAdmin && userAdmin.admin){
             const template = AdminNotiPubli(msg, image);
-            const mail = userAdmin.email;
-            const correos= await User.find({email:{$not:{$regex: '.*' + mail + '.*', $options: 'i' }}},["email"]);
+            const mail:string = userAdmin.email;
+            const correos= await User.find({email:{$not:{$regex: '.*'+mail || 'deprecated'+'.*', $options: 'i' }}} ,["email"]);
+            console.log(correos)
             correos.forEach(element => {
                 sendEmail(element.email, subject, template);
             });        
