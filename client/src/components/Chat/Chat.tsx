90cc5f244f7c9dc92a72278ff68bf3e45b0f8104
Eduardo <70926975+librotero@users.noreply.github.com>
2022-07-24 03:02:30 -0300

    actualizacion chat

--- client/src/components/Chat/Chat.tsx
+++ client/src/components/Chat/Chat.tsx
@@ -1,19 +1,25 @@
 import io from 'socket.io-client';
+import { useAppSelector } from '../../app/hooks';
+
+import { InitialState, logOut } from "../../features/user/userSlice";
 import { useState, useEffect } from 'react';
 import  ScrollToBottom from "react-scroll-to-bottom";
 import '../../scss/Chat/Chat.scss';
 const socket = io('http://localhost:5000');
 import { BiMailSend } from 'react-icons/bi';
 
+
 const Chat = () => {
   const [currentMessage, setCurrentMessage] = useState("");
   const [username, setUsername] = useState("");
   const [messageList, setMessageList] = useState<Array<any>>([]);
 
+  const user: InitialState = useAppSelector((state) => state.user);
+
   const sendMessage = () => {
     if (currentMessage !== "") {
         const messageData = {
-        author: username,
+        author: user.user,
         message: currentMessage,
         time:
             new Date(Date.now()).getHours() +
@@ -27,7 +33,8 @@ const Chat = () => {
       setCurrentMessage("");
     }
   };
-  
+
+    
   
 
   useEffect(() => {
@@ -43,17 +50,23 @@ const Chat = () => {
   }, [socket]);
   
   return (
-    <div className="chat-container">
+    <div className={user.user? "chat-container" : "blur"}>
       
         <ScrollToBottom>
           {messageList.map((messageContent : any) => {
             return (
               <div
-                id={username === messageContent.author ? "you" : "other"}
+                id={user.user === messageContent.author ? "you" : "other"}
               >
                 <div className='burbuja'>
                 <div className="author">
-                    <p id="author">{messageContent.author}</p>
+                    <p id="author">{
+                      user?
+                      user.user === messageContent.author ?
+                      "You" : messageContent.author
+                      : ""
+
+                    }</p>
                     </div>
                   <div className="message">
                     <p>{messageContent.message}</p>
@@ -68,27 +81,25 @@ const Chat = () => {
               </div>
             );
           })}
-         
-         
         </ScrollToBottom>
         <div className="bar-Send">
-           <input
-          type="text"
-          className='input-Send'
-          value={currentMessage}
-          placeholder="Message..."
-          onChange={(e) => {
-            setCurrentMessage(e.target.value);
-          }}
-          onKeyPress={(event) => {
-            event.key === "Enter" && sendMessage();
-          }}
-        />
-        <button className="btn-Send" onClick={sendMessage}>
-          <BiMailSend />
-        </button>
-        
-      </div>
+          <input
+         type="text"
+         className='input-Send'
+         value={currentMessage}
+         placeholder="Message..."
+         onChange={(e) => {
+           setCurrentMessage(e.target.value);
+         }}
+         onKeyPress={(event) => {
+           event.key === "Enter" && sendMessage();
+         }}
+       />
+       <button className="btn-Send" onClick={sendMessage}>
+         <BiMailSend />
+       </button>
+       
+     </div>
     </div>
   );
 }
