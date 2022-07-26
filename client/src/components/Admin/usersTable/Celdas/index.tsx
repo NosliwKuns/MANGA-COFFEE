07e7aa00dea0237802630f2603fb8fe36b503218
Your Namesergio-jc <sergiojara0609@gmail.com>
2022-07-26 12:29:54 -0500

    admin table all user scss

--- client/src/components/Admin/usersTable/Celdas/index.tsx
+++ client/src/components/Admin/usersTable/Celdas/index.tsx
@@ -9,6 +9,7 @@ import {
   switchBlock,
   switchStatus,
 } from "../../../../features/admin/adminSlice";
+import "../../../../scss/User/TableAllUsers.scss";
 
 const Celdas = (e: any) => {
   const [status, setStatus] = useState(e.status);
@@ -19,75 +20,98 @@ const Celdas = (e: any) => {
   const headers = useHeaders(token);
   const dispatch = useAppDispatch();
   return (
-    <div>
-      <h2>{e.users}</h2>
-      <h3>{e.email}</h3>
+    <div className="celda_user_table_users_admin">
+      <div className="celda_user_name_user">
+        <h3>{e.users}</h3>
+      </div>
+      <div className="celda_user_email">
+
+      <p>{e.email}</p>
+      </div>
       <button
+      className={status ? "button_true_table_user":"button_false_table_user"}
         onClick={async () => {
-          console.log(headers)
+          console.log(headers);
           setStatus(!status);
-          const verificated : any = await dispatch(switchStatus(headers, e.id));
-          const MySwal = withReactContent(Swal)
-        MySwal.fire({
-          html: <><h1>{verificated}</h1></>,
-            position: 'center',
-           icon: 'success',
-           showConfirmButton: false,
-           timer: 1500 ,
-          showCloseButton: true,
-          focusConfirm: false,
-          background: "#212429",
-          buttonsStyling: false,
-          customClass: {
-            confirmButton: 'confirmButton'
-          }
-        })
-        }}
-      >{`status ${status}`}</button>
-      <button
-        onClick={async () => {
-          setAdmin(!admin);
-          const verificated :any = await dispatch(switchAdmin(headers, e.id));
-          const MySwal = withReactContent(Swal)
-        MySwal.fire({
-          html: <><h1>{verificated}</h1></>,
-            position: 'center',
-           icon: 'success',
-           showConfirmButton: false,
-           timer: 1500 ,
-          showCloseButton: true,
-          focusConfirm: false,
-          background: "#212429",
-          buttonsStyling: false,
-          customClass: {
-            confirmButton: 'confirmButton'
-          }
-        })
-        }}
-      >{`admin ${admin}`}</button>
-      <button
-        onClick={async () => {
-          setBlock(!block);
-          const verificated :any= await dispatch(switchBlock(headers, e.id));
-          const MySwal = withReactContent(Swal)
+          const verificated: any = await dispatch(switchStatus(headers, e.id));
+          const MySwal = withReactContent(Swal);
           MySwal.fire({
-            html: <><h1>{verificated}</h1></>,
-              position: 'center',
-             icon: 'success',
-             showConfirmButton: false,
-             timer: 1500 ,
+            html: (
+              <>
+                <h1>{verificated}</h1>
+              </>
+            ),
+            position: "center",
+            icon: "success",
+            showConfirmButton: false,
+            timer: 1500,
             showCloseButton: true,
             focusConfirm: false,
             background: "#212429",
             buttonsStyling: false,
             customClass: {
-              confirmButton: 'confirmButton'
-            }
-          })
+              confirmButton: "confirmButton",
+            },
+          });
         }}
-      >{`block ${block}`}</button>
+      // >{`status ${status}`}</button>
+      >{`status`}</button>
+      <button
+      className={admin ? "button_true_table_user":"button_false_table_user"}
+        onClick={async () => {
+          setAdmin(!admin);
+          const verificated: any = await dispatch(switchAdmin(headers, e.id));
+          const MySwal = withReactContent(Swal);
+          MySwal.fire({
+            html: (
+              <>
+                <h1>{verificated}</h1>
+              </>
+            ),
+            position: "center",
+            icon: "success",
+            showConfirmButton: false,
+            timer: 1500,
+            showCloseButton: true,
+            focusConfirm: false,
+            background: "#212429",
+            buttonsStyling: false,
+            customClass: {
+              confirmButton: "confirmButton",
+            },
+          });
+        }}
+      // >{`admin ${admin}`}</button>
+      >{`admin`}</button>
+      <button
+      className={block ? "button_true_table_user":"button_false_table_user"}
+        onClick={async () => {
+          setBlock(!block);
+          const verificated: any = await dispatch(switchBlock(headers, e.id));
+          const MySwal = withReactContent(Swal);
+          MySwal.fire({
+            html: (
+              <>
+                <h1>{verificated}</h1>
+              </>
+            ),
+            position: "center",
+            icon: "success",
+            showConfirmButton: false,
+            timer: 1500,
+            showCloseButton: true,
+            focusConfirm: false,
+            background: "#212429",
+            buttonsStyling: false,
+            customClass: {
+              confirmButton: "confirmButton",
+            },
+          });
+        }}
+      // >{`block ${block}`}</button>
+      >{`block`}</button>
       <Link to={`/admin/msg/${e.id}`}>
-      <button>✉</button>
+        <button>✉</button>
       </Link>
     </div>
   );
