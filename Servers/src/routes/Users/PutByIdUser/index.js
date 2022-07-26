a5d37a064d9d063d5ffe3e23a28fa91f3dfc40c2
Gabriel-2310 <luisgamorales1@gmail.com>
2022-07-25 20:05:22 -0500

    update editar perfil

--- Servers/src/routes/Users/PutByIdUser/index.js
+++ Servers/src/routes/Users/PutByIdUser/index.js
@@ -22,9 +22,7 @@ const User_js_1 = __importDefault(require("../../../models/Users/User.js"));
 const router = (0, express_1.Router)();
 router.put('/update', passport_1.default.authenticate("jwt", { session: false }), (0, index_js_3.FilesImage)(), (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     var _a, _b;
-    console.log(req.files, "filesssssssssssssss");
-    // console.log(req.body, "bodyyyyyyyyyyyyyy")
-    const { users, name, lastname, user_description, telephone, address, token } = req.body;
+    const { users, name, lastname, user_description, telephone, token, country, direction, reference, postalCode } = req.body;
     const { authorization } = req.headers;
     try {
         const data = (0, index_js_2.default)(authorization);
@@ -44,13 +42,19 @@ router.put('/update', passport_1.default.authenticate("jwt", { session: false })
                 newuser_image = yield (0, index_js_1.Uploadimage)(user_image.tempFilePath, folderpath);
                 yield fs_extra_1.default.unlink(user_image.tempFilePath);
             }
+            const address = {
+                postalCode: postalCode || user.address.postalCode,
+                country: country || user.address.country,
+                direction: direction || user.address.direction,
+                reference: reference || user.address.reference
+            };
             const userUpdate = {
                 users: users || user.users,
                 name: name || user.name,
                 lastname: lastname || user.lastname,
                 user_description: user_description || user.user_description,
                 telephone: telephone || user.telephone,
-                address: address || user.address,
+                address,
                 user_banner: (newuser_banner === null || newuser_banner === void 0 ? void 0 : newuser_banner.secure_url) || user.user_banner,
                 user_image: (newuser_image === null || newuser_image === void 0 ? void 0 : newuser_image.secure_url) || user.user_image
             };
