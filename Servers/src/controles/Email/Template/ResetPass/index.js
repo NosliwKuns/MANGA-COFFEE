8328f9cf366117bba0989a3af9a050706886e568
Gabriel-2310 <luisgamorales1@gmail.com>
2022-07-27 23:52:58 -0500

    Se cierra oficialmente el back git add .git add .!

--- Servers/src/controles/Email/Template/ResetPass/index.js
+++ Servers/src/controles/Email/Template/ResetPass/index.js
@@ -2,19 +2,42 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const ResetPass = (name, id) => {
     return `
-    <div style="margin-left:21%;">
+    <div style="
+margin:40px;
+margin-left:28%;
+
+">
     <head>
         <link rel="stylesheet" href="./style.css">
     </head>
     
-    <div id="email___content" style="margin-right: 37%;" >
-    <h1>Hello ${name}</h1>
-    <h3>Reset Account Password</h3>
-    <p>There was recently a request to change the passwor for your account;</p>
-    <p>If you requested this change, set a new password here: </p>
-    <a style="margin-left: 35%; color:rgb(0, 26, 255); font-weight:bold;" href="https://manga-coffee.vercel.app/rename/password/${id}" target="_blank" > Set a New Password</a>
-    <p>If did not make this request, you can ignore this email and your password will remain the same.</p>
-           
+    <div id="email___content" style="
+    margin-right: 37%;
+    background-image: url(https://c.tenor.com/yfHWbwZUmgEAAAAC/pastel.gif);
+    border: 4px solid;
+    " >
+    <h1 style="
+    font-family: cursive;
+    margin-left:1%
+    ">Hello ${name}</h1>
+    <h3 style="
+    font-family: cursive; 
+    margin-left:1%">Reset Account Password</h3>
+    <p style="
+    font-family: cursive; 
+    margin-left:1%">There was recently a request to change the passwor for your account;</p>
+    <p style="
+    font-family: cursive; 
+    margin-left:1%">If you requested this change, set a new password here: </p>
+    <a style="
+    margin-left: 35%; 
+    font-family: cursive; 
+    color:rgb(0, 26, 255); 
+    font-weight:bold;" href="https://manga-coffee.vercel.app/rename/password/${id}" target="_blank" > Set a New Password</a>
+    <p style="
+    font-family: cursive; 
+    margin-left:1%">If did not make this request, you can ignore this email and your password will remain the same.</p>
+    <img style="margin-left:17%" src="https://i.pinimg.com/originals/e4/97/e9/e497e9cfa0c8d4c0bfd78c2c508c6f09.gif"/>        
     </div>
     </div>
     `;
