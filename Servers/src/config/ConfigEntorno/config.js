1f284bad16a66a0627d847a3716ed3dcd7358ab5
zaidraed <zaidraed@gmail.com>
2025-03-19 14:26:00 -0300

    actualizacion db info

--- Servers/src/config/ConfigEntorno/config.js
+++ Servers/src/config/ConfigEntorno/config.js
@@ -7,9 +7,9 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 exports.default = {
     //------passport-----//
-    jwtsecret: process.env.JWT_SECRET || "mysecretmangacoffetoken",
+    jwtsecret: process.env.JWT_SECRET || "defaultsecret",
     //------mongodb------//
-    Db: process.env.URI || "mongodb+srv://Default2310:dUWDSYBmgpwjgu5x@cluster0.iqdewrr.mongodb.net/Manga-Coffe",
+    Db: process.env.URI || "mongodb://localhost:27017/mangacoffe",
     //------.gooogle-----//
     MAIL: "mangacoffe2310@gmail.com",
     PASS: "manga23coffe10",
@@ -20,7 +20,7 @@ exports.default = {
     //------cloudinary----//
     CLOUDINARY_NAME: "manga-coffe",
     CLOUDINARY_API_KEY: "289919794386462",
-    CLOUDINARY_API_SECRET: "Qq6MdTg9c-yBlxlixfHI5g-LwqM"
+    CLOUDINARY_API_SECRET: "Qq6MdTg9c-yBlxlixfHI5g-LwqM",
     // https://console.cloud.google.com/apis/credentials/oauthclient/185422760918-fauj1280lc128l69nc4s5lnr47031ms7.apps.googleusercontent.com?project=correoverificacion
     //https://mail.google.com/
     //https://developers.google.com/oauthplayground/?code=4/0AdQt8qi5M9scaUnIXyPdU0IMLxtCHMFDMUi0K4E0TWtcSk58gl8z4G-jVIrHljsq62EiIA&scope=https://mail.google.com/
